8a3cd1f087f4c04a51261e9703e37a47a468585a
Colin Alexander <colintalex@gmail.com>
2020-05-06 21:52:48 -0600

    Changes symbol to string in Repo attr

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -2,7 +2,7 @@ class Repo
   attr_reader :name,
               :html_url
   def initialize(attributes)
-    @name = attributes[:name]
-    @html_url = attributes[:html_url]
+    @name = attributes['name']
+    @html_url = attributes['html_url']
   end
 end
