26537b5ee5a8dc8ceea53867be8ad2aadcb7458c
Steven Meyers <sjjmeyers@gmail.com>
2020-05-07 15:01:36 -0600

    Refactor show action in the UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,14 @@
 class UsersController < ApplicationController
   def show
-    conn = Faraday.new(url: "https://api.github.com") do |faraday|
-      faraday.headers['authorization'] = "token #{ENV["GITHUB_TOKEN"]}"
-    end
-    response = conn.get('/user/repos')
-    user_response = conn.get('/user')
-    json = JSON.parse(response.body, symbolize_names: true)
-    user_json = JSON.parse(user_response.body, symbolize_names: true)
-    @github_username = user_json[:login]
-    @repos = json.map do |repo_data|
-      Repo.new(repo_data)
-    end
+    response = GithubResponse.new
+    @repos = response.repos
+    @github_username = response.user_name
   end
-
+  
   def new
     @user = User.new
   end
-
+  
   def create
     user = User.create(user_params)
     if user.save
@@ -27,9 +19,9 @@ class UsersController < ApplicationController
       render :new
     end
   end
-
+  
   private
-
+  
   def user_params
     params.require(:user).permit(:email, :first_name, :last_name, :password)
   end
