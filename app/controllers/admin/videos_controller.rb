a6423d9133763a147325693efeb3abbf43e83f49
Colin Alexander <colintalex@gmail.com>
2020-05-10 18:25:11 -0600

    Adds to Tutorial create method, condition for creating from playlist

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -8,20 +8,34 @@ class Admin::VideosController < Admin::BaseController
     video.update(video_params)
   end
 
+  def new
+    @tutorial = Tutorial.find(params[:tutorial_id])
+  end
+
   def create
     begin
       tutorial = Tutorial.find(params[:tutorial_id])
-      thumbnail = YouTube::Video.by_id(new_video_params[:video_id]).thumbnail
-      video = tutorial.videos.new(new_video_params.merge(thumbnail: thumbnail))
-
-      video.save
-
-      flash[:success] = 'Successfully created video.'
+      if params[:playlistId].nil?
+        thumbnail = YouTube::Video.by_id(new_video_params[:video_id]).thumbnail
+        video = tutorial.videos.new(new_video_params.merge(thumbnail: thumbnail))
+        video.save
+        flash[:success] = 'Successfully created video.'
+      elsif params[:video].nil?
+        playlist = YouTube::Video.by_playlist_id(params[:playlistId])
+        playlist.each_with_index do |item, index|
+          tutorial.videos.create({title:item.title,
+                         description:item.description,
+                           thumbnail:item.thumbnail,
+                            video_id:item.id,
+                            position:index})
+        end
+      end
     rescue StandardError
       flash[:error] = 'Unable to create video.'
     end
 
-    redirect_to edit_admin_tutorial_path(id: tutorial.id)
+    # redirect_to edit_admin_tutorial_path(id: tutorial.id)
+    redirect_to admin_dashboard_path
   end
 
   private
