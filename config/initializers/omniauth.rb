18442770098621d8643f155cbada9db327e3d728
Steven Meyers <sjjmeyers@gmail.com>
2020-05-12 23:09:39 -0600

    Edit omniauth middleware

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  require 'openid/store/filesystem'
+  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: "repo"
+  provider :openid, store: OpenID::Store::Filesystem.new('/tmp')
 end
