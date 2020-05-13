8cca39ad3b2093e0df9f94e0a296bdc0a487f1e0
Steven Meyers <sjjmeyers@gmail.com>
2020-05-12 21:38:05 -0600

    Handroll github oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,8 +14,41 @@ class SessionsController < ApplicationController
     end
   end
 
+  def update
+    client_id     = ENV['GITHUB_CLIENT_ID']
+    client_secret = ENV['GITHUB_CLIENT_SECRET']
+    code          = params[:code]
+    response      = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{client_id}&client_secret=#{client_secret}&code=#{code}")
+    
+    token = extract_token(response)["access_token"]
+
+    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+
+    assign_token(response)
+    
+    redirect_to dashboard_path
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path
   end
+
+  def extract_token(response)
+    pairs = response.body.split("&")
+    response_hash = {}
+    pairs.each do |pair|
+      key, value = pair.split("=")
+      response_hash[key] = value
+    end
+    response_hash
+  end
+
+  def assign_token(response)
+    token_hash = extract_token(response)
+    token = token_hash["access_token"]
+    current_user.token = token
+    current_user.save
+  end
+
 end
