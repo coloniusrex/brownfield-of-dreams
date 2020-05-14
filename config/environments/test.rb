5116855c1e5398a45fa31c10d358ef621393e2ce
Steven Meyers <sjjmeyers@gmail.com>
2020-05-14 02:54:14 -0600

    Revert enviromnetal variable for development.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -43,4 +43,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # uncomment to run mailcatcher test locally
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
